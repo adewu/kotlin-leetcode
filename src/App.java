05eeb21f1f521eb8aaa629f0bc12e853871a7d47
wuxiaoming04 <wuxiaoming04@58.com>
2020-09-03 19:05:53 +0800

    109

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 import easy.*;
+import medium.ConvertSortedList2BinarySearchTree_109;
 import medium.IncreasingSubsequences_491;
 import medium.KthSmallestElementInSortedMatrix_378;
 import medium.MinimumSizeSubarraySum_209;
@@ -6,7 +7,7 @@ import medium.MinimumSizeSubarraySum_209;
 public class App {
 
     public static void main(String[] args) {
-        RobotReturnToOrigin_657 robotReturnToOrigin_657  = new RobotReturnToOrigin_657();
-        robotReturnToOrigin_657.exe();
+        ConvertSortedList2BinarySearchTree_109 convertSortedList2BinarySearchTree_109  = new ConvertSortedList2BinarySearchTree_109();
+        convertSortedList2BinarySearchTree_109.exe();
     }
 }
--- src/medium/ConvertSortedList2BinarySearchTree_109.kt
+++ src/medium/ConvertSortedList2BinarySearchTree_109.kt
@@ -2,11 +2,27 @@ package medium
 class ConvertSortedList2BinarySearchTree_109{
 
     fun sortedListToBST(head: ListNode?): TreeNode? {
+        if(head == null){
+            return null
+        }
+        var slow = head
+        var fast = head
+        var preSlow : ListNode ?= null
+        while (fast != null && fast.next != null){ //快慢指针
+            preSlow = slow
+            slow = slow?.next
+            fast = fast.next?.next
+        }
+        var root = TreeNode(slow!!.`val`)
 
-        //todo
-        return null
+        if (preSlow != null){
+            preSlow.next = null
+            root.left = sortedListToBST(head)
+        }
+        root.right = sortedListToBST(slow.next)
+        return root
     }
-
+    // -10 -3 0 5 9
     fun exe(){
         var root = ListNode(-10)
         root.next = ListNode(-3)
